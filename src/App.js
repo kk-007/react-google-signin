b4178b91b7d536d5378baa5fc1d3ef908c85dde9
Keval navadiya <31183263+kk-007@users.noreply.github.com>
2020-07-16 01:04:19 +0530

    signin with google in completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect} from 'react';
 
 function App() {
+  useEffect(()=>{
+    window.gapi.load('client:auth2',()=>{
+      window.gapi.client.init({
+        client_id:'210767855072-cratf6e4jb79atmqb11880oaigapoiih.apps.googleusercontent.com',
+        scope:'profile'
+      }).then(()=>{
+        window.gapi.auth2.getAuthInstance().signIn()
+        .then(user=>console.log(user.getBasicProfile().getName()))
+        .catch(err=>{
+          console.log(err.error);
+          switch(err.error){
+            case 'popup_closed_by_user':
+              window.alert('Sign In for Continue');
+              break;
+            case 'access_denied':
+              window.alert('Detail Required for Reference');
+              break;
+            case 'popup_blocked_by_browser':
+              window.alert('allow popup for sign-in with google');
+              break;
+            default:
+              window.alert('System Error',err.error)
+              break;
+          }
+        });
+      })
+    });
+  },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello
     </div>
   );
 }
