5aa6d5b7f75cf0c60f4f4f37c05f9fb3250425e8
Keval navadiya <31183263+kk-007@users.noreply.github.com>
2020-07-16 03:10:04 +0530

    change the async await

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import React,{useEffect} from 'react';
+import React from 'react';
 import { useSelector,useDispatch } from 'react-redux';
 import { signIn,signOut } from "./actions/auth";
 function App() {
   let dispatch = useDispatch();
   let auth = useSelector(state=>state).auth;
-  useEffect(()=>{
-    window.gapi.load('client:auth2',()=>{
-      window.gapi.auth2.init({
-        client_id:'210767855072-cratf6e4jb79atmqb11880oaigapoiih.apps.googleusercontent.com',
-        scope:'profile'
-      })
-    });
-  },[]);
   return (
     <div>
       {auth.isSignIn?'Welcome '+auth.Name:''}
